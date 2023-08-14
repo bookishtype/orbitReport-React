f7f1ccc06d525f09bf7fb8756aee4cc768282345
Faith <112731014+bookishtype@users.noreply.github.com>
2023-08-14 11:21:37 -0500

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -15,9 +15,9 @@ const Table = ({ sat }) => {
         </tr>
         </thead>
         <tbody>
-    //The body of the table will use the map function to render the data based on which button a user selects.
+
           {sat.map( (data, id) =>{
-            //Iterate through the sat prop using map; use the following callbacks: id and data. returns data to each row in the table.
+        
             return (<tr key={id}>
               <td>{data.name}</td>
               <td>{data.type}</td>
