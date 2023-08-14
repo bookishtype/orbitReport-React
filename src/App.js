e96eefad68acad1c3faf390468500ed5844ec5b6
Faith <112731014+bookishtype@users.noreply.github.com>
2023-08-14 10:49:12 -0500

    comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+//task 2
 import Banner from "./components/Banner";
+//task 3
 import { useState } from "react";
 import satData from "./components/satData";
 
 function App() {
-
+//task 3
   const [sat, setSat] = useState(satData);
+  //creates new array. Used to create buttons
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
-
+//task 3 arrow function: Filters array until it finds the data it wants
   const filterByType = (currentType) =>{
     const displaySats = satData.filter((newSatDisplay) => {
       return newSatDisplay.orbitType === currentType;
